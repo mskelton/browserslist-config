3a64e93d0968c830ba11bcf35e0f8cb91ecc4fa6
Mark Skelton <mdskelton99@gmail.com>
2022-01-06 07:33:43 -0600

    feat: Modernize browsers

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 module.exports = [
-  "last 4 chrome versions",
-  "last 4 edge versions",
-  "last 4 firefox versions",
-  "last 4 safari versions",
+  "last 3 chrome versions",
+  "last 3 edge versions",
+  "last 3 firefox versions",
+  "last 2 safari versions",
 ];
